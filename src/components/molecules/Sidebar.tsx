0d3b76d707731a94cb6f3c1008fcab3df4796aaf
SebasthianSalpozz <sebasthiansalinaspozzo@gmail.com>
2025-09-14 17:33:11 -0400

    feat: add sidebar and audit card

--- src/components/molecules/Sidebar.tsx
+++ src/components/molecules/Sidebar.tsx
@@ -1,37 +1,116 @@
-import type { ReactNode } from "react";
-import { useNavigate } from "react-router-dom";
+import PersonIcon from "@mui/icons-material/Person";
+import { useState, type ReactNode } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { useSideBarStore } from "../../Zustand/stores/SidebarStore";
+import { useAuthStore } from "../../Zustand/stores/AuthStore";
+import DialogTemplate from "../templates/DialogTemplate";
+import { useDialogStore } from "../../Zustand/stores/DialogStore";
+import CustomizeForm from "../organinsms/CustomizeForm";
+import { useToastStore } from "../../Zustand/stores/ToastStore";
+import AppLogo from "../../assets/AppLogo";
 
 interface Props {
   children: ReactNode;
-  navigationList: {
-    label: string
-    redirection: string
-  }[]
+  role: string | null;
 }
 
-export default function Sidebar({ children, navigationList }: Props) {
+export default function Sidebar({ children, role }: Props) {
+  const { tenantId } = useParams();
   const navigate = useNavigate();
+  const { sideBarItems: navigationList } = useSideBarStore();
+  const { logout } = useAuthStore();
+  const [isUserOptionsOpen, setIsUserOptionsOpen] = useState(false);
+  const { setDialogStatus, getDialogStatus } = useDialogStore();
+  const { executeToastOnClose } = useToastStore();
 
   const changeScreen = (path: string) => navigate(path);
 
   return (
-    <div className="drawer drawer-open">
-      <input type="checkbox" className="drawer-toggle" />
-      <div className="drawer-content flex flex-col bg-base-200 ">
+    <div className="flex flex-col">
+      <nav className="flex header min-h-full w-full px-4 items-center justify-between">
+        <div className="flex items-center gap-2">
+          <figure className="flex items-center break-words w-48">
+            <AppLogo />
+            <figcaption className="font-semibold text-center">
+              Sistema de Catalogación
+            </figcaption>
+          </figure>
+          <ul className="flex gap-4">
+            {navigationList.map((item, i) => (
+              <li
+                key={i}
+                className="hover:font-semibold active:font-bold cursor-pointer"
+              >
+                <a onClick={() => changeScreen(item.redirection)}>
+                  {item.label}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="relative flex items-center gap-2">
+          <div
+            className="avatar avatar-placeholder cursor-pointer"
+            onClick={() => setIsUserOptionsOpen(!isUserOptionsOpen)}
+          >
+            <div className="bg-primary text-neutral-content w-8 rounded-full">
+              <span className="text-xs">
+                <PersonIcon />
+              </span>
+            </div>
+          </div>
+          {isUserOptionsOpen && (
+            <div className="absolute right-0 top-10 bg-base-100 shadow-lg p-2 whitespace-nowrap z-90 flex flex-col rounded-xl gap-1 w-32">
+              {role === "Director" && (
+                <>
+                  <button
+                    className="cursor-pointer hover:font-semibold hover:bg-base-200 px-2 py-1 rounded transition-colors col-span-1 w-full"
+                    onClick={() => {
+                      executeToastOnClose("customizeDialog");
+                      setIsUserOptionsOpen(false);
+                      setDialogStatus("customizeDialog", true);
+                    }}
+                  >
+                    Personalizar
+                  </button>
+                  <div className="w-full border-b border-terracota" />
+                </>
+              )}
+              <button
+                type="button"
+                className="cursor-pointer hover:font-semibold hover:bg-base-200 px-2 py-1 rounded transition-colors col-span-1"
+                onClick={() => {
+                  setIsUserOptionsOpen(false);
+                  logout();
+                  if (tenantId) {
+                    navigate(`/${tenantId}/login`);
+                  } else {
+                    navigate("/login");
+                  }
+                }}
+              >
+                Cerrar Sesión
+              </button>
+            </div>
+          )}
+        </div>
+      </nav>
+      <div
+        className={`flex flex-col bg-base-200 h-full min-h-[91vh] ${
+          getDialogStatus("customizeDialog") && "overflow-hidden max-h-[91vh]"
+        }`}
+      >
         {children}
       </div>
-      <nav className="drawer-side">
-        <ul className="menu bg-base-400 min-h-full w-60 p-4 text-base-100">
-          {navigationList.map((item, i) => (
-            <li
-              key={i}
-              className="hover:bg-base-100 hover:text-base-400 rounded-md active:bg-base-100"
-            >
-              <a onClick={() => changeScreen(item.redirection)}>{item.label}</a>
-            </li>
-          ))}
-        </ul>
-      </nav>
+      <DialogTemplate
+        dialogId="customizeDialog"
+        onClose={() => {
+          setDialogStatus("customizeDialog", false);
+        }}
+        variant="long"
+      >
+        <CustomizeForm />
+      </DialogTemplate>
     </div>
   );
 }
