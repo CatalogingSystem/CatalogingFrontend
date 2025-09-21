570421327446932c9de37cd3b1dbde06ada8f295
SebasthianSalpozz <sebasthiansalinaspozzo@gmail.com>
2025-09-21 17:00:32 -0400

    feat: add tenant models

--- src/models/TenantCreate.model.ts
+++ src/models/TenantCreate.model.ts
@@ -1,13 +1,16 @@
-import {z} from 'zod'
+import { z } from "zod";
 
-const abreviationsRegex = new RegExp(/^(?=(?:[A-Z0-9]*-)*[A-Z0-9]*$)(?:[A-Z0-9]-?){1,10}$/)
-const countryRegex = new RegExp(/^[A-Z]{2}$/)
+const abreviationsRegex = new RegExp(
+  /^(?=(?:[A-Z0-9]*-)*[A-Z0-9]*$)(?:[A-Z0-9]-?){1,10}$/
+);
+const countryRegex = new RegExp(/^[A-Z]{2}$/);
 
 export const TenantSchema = z.object({
   country: z.string().regex(countryRegex),
   abrevations: z.string().trim().regex(abreviationsRegex),
   name: z.string().min(1).max(100).trim(),
-  description: z.string().min(1).trim()
-})
+  description: z.string().min(1).trim(),
+  imageUrl: z.any().optional().nullable(),
+});
 
-export type TenantFormValues = z.infer<typeof TenantSchema>
+export type TenantFormValues = z.infer<typeof TenantSchema>;
